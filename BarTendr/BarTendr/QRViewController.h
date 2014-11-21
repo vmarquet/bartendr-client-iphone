3893d429d252d0771b2ceb7764dc0194067e62c5
Patouz <patouz@MacBook-de-Patouz.local>
2014-11-21 18:38:29 +0100

    ajout du controller sur la view du QR code, ajout de 2 catégorie de boissons en manuel

---
+++ BarTendr/BarTendr/QRViewController.h
@@ -0,0 +1,15 @@
+//
+//  QRViewController.h
+//  BarTendr
+//
+//  Created by Patouz on 21/11/2014.
+//  Copyright (c) 2014 Patouz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *message_QR;
+
+@end
---
+++ BarTendr/BarTendr/QRViewController.m
@@ -0,0 +1,38 @@
+//
+//  QRViewController.m
+//  BarTendr
+//
+//  Created by Patouz on 21/11/2014.
+//  Copyright (c) 2014 Patouz. All rights reserved.
+//
+
+#import "QRViewController.h"
+
+@interface QRViewController ()
+
+@end
+
+@implementation QRViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"Deuxième vue !");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
