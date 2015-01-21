d583c2b2a12cfc663d3e9d9b5b87ce0779f48a2a
Thibaut <tdasilva@et.esiea.fr>
2015-01-21 15:21:27 +0100

    CustomCell table des catégories

---
+++ BarTendr/BarTendr/CategoryCell.h
@@ -0,0 +1,13 @@
+//
+//  CategoryCell.h
+//  BarTendr
+//
+//  Created by ESIEA on 21/01/2015.
+//  Copyright (c) 2015 Patouz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryCell : UITableViewCell
+
+@end
---
+++ BarTendr/BarTendr/CategoryCell.m
@@ -0,0 +1,23 @@
+//
+//  CategoryCell.m
+//  BarTendr
+//
+//  Created by ESIEA on 21/01/2015.
+//  Copyright (c) 2015 Patouz. All rights reserved.
+//
+
+#import "CategoryCell.h"
+
+@implementation CategoryCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
